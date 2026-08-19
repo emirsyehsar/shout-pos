28923c4bab684d0c95368d201af254eda69d3c84
emirsyehsar <emir.syehsar@gmail.com>
2026-08-19 17:28:04 +0700

    Added basic button

--- app/src/main/java/com/emirst/shoutpos/MainActivity.kt
+++ app/src/main/java/com/emirst/shoutpos/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.emirst.shoutpos
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.emirst.shoutpos.ui.theme.ShoutposTheme
 
@@ -20,10 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShoutposTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    PressButtonScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +34,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PressButtonScreen(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val toastText = stringResource(R.string.press_button_toast)
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Button(onClick = {
+            Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show()
+        }) {
+            Text(text = stringResource(R.string.press_button))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PressButtonScreenPreview() {
     ShoutposTheme {
-        Greeting("Android")
+        PressButtonScreen()
     }
 }
